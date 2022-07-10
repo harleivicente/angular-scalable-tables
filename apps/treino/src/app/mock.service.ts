dec1056283e92aa353edaa9555c4440349f7fb5a
Harlei Vicente <harleivicente@gmail.com>
2022-07-10 16:22:00 -0300

    update

--- apps/treino/src/app/mock.service.ts
+++ apps/treino/src/app/mock.service.ts
@@ -32,7 +32,7 @@ export class MockService {
   constructor() {}
 
   getPeople(filter: Filter): Observable<Result<Person>> {
-    const delay = 100;
+    const delay = 350;
     return new Observable(observer => {
       setTimeout(() => {
         const result = {
